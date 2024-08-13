6bc442f8df788a787f7306a15b635ba2b6956161
MartinXCVI <mdzg96@gmail.com>
2024-08-12 23:22:25 -0300

    fix in works cards

--- src/data/WorksData.jsx
+++ src/data/WorksData.jsx
@@ -3,63 +3,63 @@ export default [
     id: 1,
     link: 'https://www.google.com',
     image: '/images/works-1.jpg',
-    title: 'Lonely Path',
-    subtitle: 'Web Design'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   },
   {
     id: 2,
     link: 'https://www.google.com',
     image: '/images/works-2.jpg',
-    title: 'Photographer Girl',
-    subtitle: 'Branding'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   },
   {
     id: 3,
     link: 'https://www.google.com',
     image: '/images/works-3.jpg',
-    title: 'The Difference',
-    subtitle: 'Web Design'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   },
   {
     id: 4,
     link: 'https://www.google.com',
     image: '/images/works-4.jpg',
-    title: 'Nature Patterns',
-    subtitle: 'Branding'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   },
   {
     id: 5,
     link: 'https://www.google.com',
     image: '/images/works-5.jpg',
-    title: 'The Difference',
-    subtitle: 'Photography'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   },
   {
     id: 6,
     link: 'https://www.google.com',
     image: '/images/works-6.jpg',
-    title: 'Winter Sonata',
-    subtitle: 'Web Design'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   },
   {
     id: 7,
     link: 'https://www.google.com',
     image: '/images/works-7.jpg',
-    title: 'Lonely Path',
-    subtitle: 'Branding'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   },
   {
     id: 8,
     link: 'https://www.google.com',
     image: '/images/works-8.jpg',
-    title: 'Appreciation',
-    subtitle: 'Photography'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   },
   {
     id: 9,
     link: 'https://www.google.com',
     image: '/images/works-9.jpg',
-    title: 'Happy Days',
-    subtitle: 'Web Design'
+    title: 'Title Here',
+    subtitle: 'Short Description Here'
   }
 ]
